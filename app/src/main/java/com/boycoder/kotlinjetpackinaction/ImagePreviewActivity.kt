d93042e0a85e3d9ddd65a30eae0ad761cc81a3fc
油炸电视剧 <844808402@qq.com>
2021-08-19 23:28:37 +0800

    书写Kotlin相关代码

--- app/src/main/java/com/boycoder/kotlinjetpackinaction/ImagePreviewActivity.kt
+++ app/src/main/java/com/boycoder/kotlinjetpackinaction/ImagePreviewActivity.kt
@@ -1,28 +1,20 @@
-package com.boycoder.kotlinjetpackinaction;
+package com.boycoder.kotlinjetpackinaction
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.widget.ImageView;
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.bumptech.glide.Glide;
-
-public class ImagePreviewActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image_preview);
-
-        Intent intent = getIntent();
-        String url = intent.getStringExtra(MainActivity.EXTRA_PHOTO);
+class ImagePreviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_preview)
+        val intent = intent
+        val url = intent.getStringExtra(MainActivity.EXTRA_PHOTO)
         if (!TextUtils.isEmpty(url)) {
-            ImageView imageView = findViewById(R.id.imagePreview);
-            Glide.with(this).load(url).into(imageView);
+            val imageView = findViewById<ImageView>(R.id.imagePreview)
+            Glide.with(this).load(url).into(imageView)
         }
     }
-
-
 }
--- app/src/main/java/com/boycoder/kotlinjetpackinaction/MainActivity.kt
+++ app/src/main/java/com/boycoder/kotlinjetpackinaction/MainActivity.kt
@@ -1,109 +1,88 @@
-package com.boycoder.kotlinjetpackinaction;
+package com.boycoder.kotlinjetpackinaction
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.boycoder.kotlinjetpackinaction.MainActivity
+import com.boycoder.kotlinjetpackinaction.entity.User
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.gson.Gson
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.boycoder.kotlinjetpackinaction.entity.User;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.google.gson.Gson;
+    lateinit var stringRequest: StringRequest
+    lateinit var requestQueue: RequestQueue
 
-public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "Main";
-    public static final String EXTRA_PHOTO = "photo";
-
-    StringRequest stringRequest;
-    RequestQueue requestQueue;
-
-    private ImageView image;
-    private ImageView gif;
-    private TextView username;
-    private TextView company;
-    private TextView website;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        init();
+    private var image: ImageView? = null
+    private var gif: ImageView? = null
+    private var username: TextView? = null
+    private var company: TextView? = null
+    private var website: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
     }
 
-    private void init() {
-        image = findViewById(R.id.image);
-        gif = findViewById(R.id.gif);
-        username = findViewById(R.id.username);
-        company = findViewById(R.id.company);
-        website = findViewById(R.id.website);
-
-        display(User.CACHE_RESPONSE);
-        requestOnlineInfo();
+    private fun init() {
+        image = findViewById(R.id.image)
+        gif = findViewById(R.id.gif)
+        username = findViewById(R.id.username)
+        company = findViewById(R.id.company)
+        website = findViewById(R.id.website)
+        display(User.CACHE_RESPONSE)
+        requestOnlineInfo()
     }
 
-    private void requestOnlineInfo() {
-        requestQueue = Volley.newRequestQueue(this);
-        String url ="https://api.github.com/users/JakeWharton";
-        stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        display(response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-        stringRequest.setTag(TAG);
-        requestQueue.add(stringRequest);
+    private fun requestOnlineInfo() {
+        requestQueue = Volley.newRequestQueue(this)
+        val url = "https://api.github.com/users/JakeWharton"
+        stringRequest = StringRequest(Request.Method.GET, url,
+                { response -> display(response) }) { error -> Toast.makeText(this@MainActivity, error.message, Toast.LENGTH_SHORT).show() }
+        stringRequest!!.tag = TAG
+        requestQueue.add(stringRequest)
     }
 
-    private void display(@Nullable String response) {
-        if (TextUtils.isEmpty(response)) { return; }
-
-        Gson gson = new Gson();
-        final User user = gson.fromJson(response, User.class);
-        if (user != null){
-            Glide.with(this).load("file:///android_asset/bless.gif").into(gif);
-            Glide.with(this).load(user.getAvatar_url()).apply(RequestOptions.circleCropTransform()).into(image);
-            this.username.setText(user.getName());
-            this.company.setText(user.getCompany());
-            this.website.setText(user.getBlog());
-
-            image.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    gotoImagePreviewActivity(user);
-                }
-            });
+    private fun display(response: String?) {
+        if (TextUtils.isEmpty(response)) {
+            return
+        }
+        val gson = Gson()
+        val user = gson.fromJson(response, User::class.java)
+        if (user != null) {
+            Glide.with(this).load("file:///android_asset/bless.gif").into(gif!!)
+            Glide.with(this).load(user.avatar_url).apply(RequestOptions.circleCropTransform()).into(image!!)
+            username!!.text = user.name
+            company!!.text = user.company
+            website!!.text = user.blog
+            image!!.setOnClickListener { gotoImagePreviewActivity(user) }
         }
     }
 
-    private void gotoImagePreviewActivity(User user) {
-        Intent intent = new Intent(this, ImagePreviewActivity.class);
-        intent.putExtra(EXTRA_PHOTO, user.getAvatar_url());
-        startActivity(intent);
+    private fun gotoImagePreviewActivity(user: User) {
+        val intent = Intent(this, ImagePreviewActivity::class.java)
+        intent.putExtra(EXTRA_PHOTO, user.avatar_url)
+        startActivity(intent)
     }
 
-    @Override
-    protected void onStop () {
-        super.onStop();
+    override fun onStop() {
+        super.onStop()
         if (requestQueue != null) {
-            requestQueue.cancelAll(TAG);
+            requestQueue!!.cancelAll(TAG)
         }
+    }
+
+    companion object {
+        const val TAG = "Main"
+        const val EXTRA_PHOTO = "photo"
     }
 }
